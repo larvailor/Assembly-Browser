62ad4e20711623bdae4eb6bf611ce6ab7178b759
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 12:56:27 +0300

    development; add: Method model for handling method info

---
+++ Assembly-Browser/Model/Method.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Method
+    {
+        // variables
+        public string Name { get; private set; }
+        public string Attributes { get; private set; }
+        public string ReturnType { get; private set; }
+        public string[] Parameters { get; }
+
+
+
+        // methods
+        public Method(string attributes, string returnType, string name, string[] parameters)
+        {
+            Name = name;
+            Attributes = attributes;
+            ReturnType = returnType;
+            Parameters = parameters;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Attributes + " " + ReturnType + " " + Name + "(" + String.Join(", ", Parameters) + ")";
+        }
+    }
+}
