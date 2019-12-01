be6700ebed9579a956a7f55187961fc58869a093
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 23:46:24 +0300

    development; add: TestAssembly project, upd: tests

--- Assembly-Browser/Tests/ABtests.cs
+++ Assembly-Browser/Tests/ABtests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Model;
+using Model.Assembly;
+using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
@@ -6,9 +9,20 @@ namespace Tests
     [TestClass]
     public class ABtests
     {
+        public string pathToDll()
+        {
+            return System.IO.Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\dll\\TestAssembly.dll");
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            AssemblyBrowser ab = AssemblyLoader.Load(pathToDll());
+            Assert.IsTrue(ab.Name.StartsWith("TestAssembly"));
+            Assert.AreEqual(ab.Namespaces.Count, 1);
+            Assert.AreEqual(ab.Namespaces["TestAssembly"].Name, "TestAssembly");
+            Assert.AreEqual(ab.Namespaces["TestAssembly"].Types.Count, 1);
+            Assert.AreEqual(ab.Namespaces["TestAssembly"].Types[0].Name, "TestAssembly.Class12");
         }
     }
 }
