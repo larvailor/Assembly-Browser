2b10defeeb913ae9b480b896185ce07cd5c332c4
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 23:46:24 +0300

    development; add: TestAssembly project, upd: tests

---
+++ Assembly-Browser/TestAssembly/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssembly
+{
+    public class Class1
+    {
+        // variables
+        private int field;
+        public int Property1 { set; get; }
+
+
+
+        // methods
+        public string RturnStringMethods(int x = 1)
+        {
+            field = x;
+            return "Class1";
+        }
+
+    }
+}
