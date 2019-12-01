61267f3e187c968d6fac2ac0504d15104b1f18ca
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 14:08:59 +0300

    development; Model project structure more oranized

--- Assembly-Browser/Model/Assembly/Namespace/ExportedNsInfo.cs
+++ Assembly-Browser/Model/Assembly/Namespace/ExportedNsInfo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Model.Assembly.Namespace.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Model.Assembly.Namespace
 {
     public class ExportedNsInfo
     {
--- Assembly-Browser/Model/Assembly/Namespace/types/ExportedTypeInfo.cs
+++ Assembly-Browser/Model/Assembly/Namespace/types/ExportedTypeInfo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Model.Assembly.Namespace.Types.Members;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Model.Assembly.Namespace.Types
 {
     public class ExportedTypeInfo
     {
