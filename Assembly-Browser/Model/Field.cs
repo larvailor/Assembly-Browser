8bb1f89e566f7751fe5c635da5c4a61ac41bfaa6
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 12:47:30 +0300

    development; add: Field model for handling field info

---
+++ Assembly-Browser/Model/Field.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Field
+    {
+        // methods
+        public string Name { get; private set; }
+        public string Attributes { get; private set; }
+        public string Type { get; private set; }
+
+
+
+        // methods
+        public Field(string name, string attributes, string type)
+        {
+            Name = name;
+            Attributes = attributes;
+            Type = type;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Attributes + " " + Type + " " + Name;
+        }
+    }
+}
