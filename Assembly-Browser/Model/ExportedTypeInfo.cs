38041645cc47c51de683ab08eec82c768d7a1d28
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 13:18:51 +0300

    development; add: ExportedTypeInfo model
    Used for handling all info about exported type in current namespace in
    assembly

---
+++ Assembly-Browser/Model/ExportedTypeInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ExportedTypeInfo
+    {
+        // variables
+        public string Name { get; private set; }
+        public string Attributes { get; private set; }
+        public List<Field> Fields { get; private set; }
+        public List<Property> Properties { get; private set; }
+        public List<Method> Methods { get; private set; }
+
+
+
+        // methods
+        public ExportedTypeInfo(string name, string attributes, List<Field> fields, List<Property> properties, List<Method> methods)
+        {
+            Name = name;
+            Attributes = attributes;
+            Fields = fields;
+            Properties = properties;
+            Methods = methods;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Attributes + " " + Name;
+        }
+    }
+}
