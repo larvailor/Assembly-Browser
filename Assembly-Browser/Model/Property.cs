f86f767c10a70a5af727f5610846e038d5b9cb8d
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 12:51:41 +0300

    development; add: Property model for handling property info

---
+++ Assembly-Browser/Model/Property.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Property
+    {
+        // variables
+        public string Name { get; private set; }
+        public string SetMethod { get; private set; }
+        public string GetMethod { get; private set; }
+
+
+
+        // methods
+        public Property(string name, string setMethod, string getMethod)
+        {
+            Name = name;
+            SetMethod = setMethod;
+            GetMethod = getMethod;
+        }
+
+
+
+        public override string ToString()
+        {
+            string result = Name;
+            if (GetMethod != null)
+            {
+                result += "{ " + GetMethod + " get; }";
+            }
+
+            if (SetMethod != null)
+            {
+                result += "{ " + SetMethod + " set; }";
+            }
+
+            return result;
+        }
+    }
+}
