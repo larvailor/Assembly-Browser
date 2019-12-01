ccfa53da37abca7c8aa926f383493fbf99db6252
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 14:08:59 +0300

    development; Model project structure more oranized

--- Assembly-Browser/Model/Assembly/AssemblyBrowser.cs
+++ Assembly-Browser/Model/Assembly/AssemblyBrowser.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Model.Assembly.Namespace;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Model.Assembly
 {
     public class AssemblyBrowser
     {
