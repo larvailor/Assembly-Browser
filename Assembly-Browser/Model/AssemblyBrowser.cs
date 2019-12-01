9d4e6483b54bc5e9f397c45a86ef0bc35087a1c9
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 13:42:05 +0300

    development; add: AssemblyBrowser model
    Used for handling all info in assembly

---
+++ Assembly-Browser/Model/AssemblyBrowser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class AssemblyBrowser
+    {
+        // variables
+        public string Name { get; private set; }
+        public Dictionary<string, ExportedNsInfo> Namespaces { get; }
+
+
+
+        // methods
+        public AssemblyBrowser(string name, Dictionary<string, ExportedNsInfo> namespaces)
+        {
+            Name = name;
+            Namespaces = namespaces;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Assembly " + Name;
+        }
+
+    }
+}
