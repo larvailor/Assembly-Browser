5dddeb47b498f6c592f0bdfe0bd6515f9dcbf338
Dima Konovko <dima.konovko@mail.ru>
2019-12-01 13:34:35 +0300

    developemnt; add: ExportedNsInfo model
    Used for handling all info about namespace in assembly

---
+++ Assembly-Browser/Model/ExportedNsInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ExportedNsInfo
+    {
+        // variables
+        public string Name { get; set; }
+        public List<ExportedTypeInfo> Types { get; private set; }
+
+
+
+        // methods
+        public ExportedNsInfo(string name, List<ExportedTypeInfo> types)
+        {
+            Name = name;
+            Types = types;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "NS: " + Name;
+        }
+    }
+}
