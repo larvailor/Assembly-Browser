fd3da52f8088f510ae0932088c3fb7dbe9c72122
Dima Konovko <dima.konovko@mail.ru>
2019-12-04 00:52:21 +0300

    development; ViewModel finished, add: new empty project for view

---
+++ Assembly-Browser/ViewModel/TreeNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class TreeNode : INotifyPropertyChanged
+    {
+        // variables
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<TreeNode> _nodes;
+        public ObservableCollection<TreeNode> Nodes
+        {
+            get { return _nodes; }
+            set
+            {
+                _nodes = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        // methods
+        public TreeNode() { }
+        public TreeNode(string name, ObservableCollection<TreeNode> nodes = null)
+        {
+            Name = name;
+            Nodes = nodes;
+        }
+
+
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
