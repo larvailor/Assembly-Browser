8d527c9b5fa2bbe08fbed223493bc5d3fffcc36c
Dima Konovko <dima.konovko@mail.ru>
2019-12-04 01:10:09 +0300

    development; rm: prev View projects, add: new WPF proj

---
+++ Assembly-Browser/View2/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ViewModel;
+
+namespace AssemblyBrowser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new MainView();
+            DataContext = viewModel;
+            var commandBinding = new CommandBinding();
+            commandBinding.Command = ApplicationCommands.Open;
+            commandBinding.Executed += viewModel.LoadAssembly;
+            menuItem_Open.CommandBindings.Add(commandBinding);
+        }
+    }
+}
