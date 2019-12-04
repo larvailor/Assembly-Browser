fff83fe06ecc97fd2620f5803362dc2945f5eb9a
Dima Konovko <dima.konovko@mail.ru>
2019-12-04 14:26:01 +0300

    development; upd: TestAssembly with more information

--- Assembly-Browser/TestAssembly/Class1.cs
+++ Assembly-Browser/TestAssembly/Class1.cs
@@ -9,8 +9,10 @@ namespace namespace1
     public class Class1
     {
         // variables
-        public int Property1 { get; set; }
-        public int Property2 { get; private set; }
+        public float publicField;
+        private float privateField;
+        public int PublicProperty { get; set; }
+        public int PropertyWithPrivateSet { get; private set; }
 
 
 
@@ -20,5 +22,18 @@ namespace namespace1
             return "Class1";
         }
 
+
+
+        public int MethodWithParams(int x, double y)
+        {
+            return 1;
+        }
+
+
+
+        private void PrivateMethod()
+        {
+
+        }
     }
 }
